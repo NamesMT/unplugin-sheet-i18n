e4e1852d6db1d39a34c7aec74a4216f932f1430c
Trung Dang <trung.dangquoc@gameloft.com>
2023-12-03 18:13:29 +0700

    refactor!: switch logger to `unjs/consola`

--- src/logger.ts
+++ src/logger.ts
@@ -1,29 +1,8 @@
-import { createRequire } from 'node:module'
-import { isColorSupported, isDevelopment } from 'std-env'
-import pino from 'pino'
+import { isDevelopment } from 'std-env'
+import { LogLevels, createConsola } from 'consola'
 
-const require = globalThis.require || createRequire(import.meta.url)
-
-const prettyExists = (() => {
-  try {
-    require('pino-pretty')
-    return true
-  }
-  catch {
-    return false
-  }
-})()
-
-export const logger = pino(
+export const logger = createConsola(
   {
-    level: isDevelopment ? 'debug' : 'info',
-    base: undefined,
-    transport: isDevelopment && prettyExists
-      ? { target: 'pino-pretty', options: { colorize: isColorSupported } }
-      : undefined,
-    // redact: {
-    //   paths: [],
-    //   remove: true,
-    // },
+    level: isDevelopment ? LogLevels.debug : undefined,
   },
 )
