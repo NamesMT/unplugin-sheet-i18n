b7b9a73ccbdd5d10723b15f903bde4398796d263
Trung Dang <trung.dangquoc@gameloft.com>
2023-12-03 18:11:35 +0700

    fix: `pino-pretty` not found

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,24 @@
+import { createRequire } from 'node:module'
 import { isColorSupported, isDevelopment } from 'std-env'
 import pino from 'pino'
+
+const require = globalThis.require || createRequire(import.meta.url)
+
+const prettyExists = (() => {
+  try {
+    require('pino-pretty')
+    return true
+  }
+  catch {
+    return false
+  }
+})()
 
 export const logger = pino(
   {
     level: isDevelopment ? 'debug' : 'info',
     base: undefined,
-    transport: isDevelopment
+    transport: isDevelopment && prettyExists
       ? { target: 'pino-pretty', options: { colorize: isColorSupported } }
       : undefined,
     // redact: {
