fc30f2c520e0a0ca63cdae37b1f65cea24e14e1e
NamesMT <dangquoctrung123@gmail.com>
2024-06-05 10:09:46 +0000

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,25 @@
-import unocss from '@unocss/eslint-config/flat'
 import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    unocss: true,
     ignores: [
       // eslint ignore globs here
     ],
   },
   {
     rules: {
+      // Allow trailing space in comments, for possible JSDoc formattings
       'style/no-trailing-spaces': ['error', { ignoreComments: true }],
+      // Relaxes inline statements a bit
       'style/max-statements-per-line': ['error', { max: 2 }],
     },
   },
+  // Allow trailing space for markdown formatting
   {
-    files: ['*.md'],
+    files: ['**/*.md'],
     rules: {
       'style/no-trailing-spaces': 'off',
     },
   },
-  unocss,
 )
